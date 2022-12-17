24dc405e77547bf84f004fe315edc0243df0abb0
tristanpank <tristanpank@gmail.com>
2022-12-17 10:58:13 -0500

    Make submit change div text

--- weather-api.js
+++ weather-api.js
@@ -6,12 +6,19 @@ const getWeatherData = async (city) => {
     // console.log(response);
     let data = await response.json();
     // console.log(data);
+    console.log(data);
     let specificData = {
         "name": data.name,
         "main": data.main,
         "clouds": data.clouds,
+        "wind": data.wind,
+        "wind_speed": data.wind.speed,
+        "temp": convertToFarenheight(data.main.temp),
+        "temp_min": convertToFarenheight(data.main.temp_min),
+        "temp_max": convertToFarenheight(data.main.temp_min),
+        "humidity": `${data.main.humidity}%`,
     }
-
+    
     return specificData;
     // let kelvinTemp = data.main.temp;
     // let farenheightTemp = (kelvinTemp - 273.15) * (9/5) + 32;
@@ -34,10 +41,33 @@ const getWeatherData2 = (city) => {
     })
 }
 
-let weatherTest = document.getElementById("weather-test");
-let submitButton = document.getElementById("submit");
-submitButton.addEventListener('click', async () => {
-    let cityInput = document.getElementById("city");
-    let weatherData = await getWeatherData("Ocala");
-    console.log(weatherData);
-})
+function convertToFarenheight(kelvin) {
+    return ((kelvin - 273.15) * (9/5) + 32).toFixed(2);
+}
+
+
+function main() {
+    let weatherTest = document.getElementById("weather-test");
+    let submitButton = document.getElementById("submit");
+    let weatherText = document.querySelectorAll(".weather-text");
+    for (let i=0; i < weatherText.length; i++){
+        weatherText[i].textContent = weatherText[i].id;
+    }
+    submitButton.addEventListener('click', async (event) => {
+        event.preventDefault();
+        let cityInput = document.getElementById("city");
+        try{
+            let weatherData = await getWeatherData(cityInput.value);
+            console.log(weatherData);
+            console.log(convertToFarenheight(weatherData.main.temp));
+            for (let i = 0; i < weatherText.length; i++){
+                weatherText[i].textContent = weatherData[weatherText[i].id];
+            }
+        }
+        catch (err){
+            console.log(err);
+        }
+    })
+}
+
+main();
